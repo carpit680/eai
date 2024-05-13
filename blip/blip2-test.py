98f7ffd1db125c719c4f644ab02a4a2cf393d4ca
carpit680 <carpit680@gmail.com>
2024-05-13 17:41:39 +0530

    added blip2, kosmos2, groq with llama3

---
+++ blip/blip2-test.py
@@ -0,0 +1,34 @@
+import json
+import torch
+import requests
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration, AutoConfig
+
+MODEL_PATH = "Salesforce/blip-image-captioning-large"
+
+with open('blip2-config.json', 'r') as file:
+    config_dict = json.load(file)
+
+
+config = AutoConfig.from_pretrained(MODEL_PATH,**config_dict)
+
+processor = BlipProcessor.from_pretrained(MODEL_PATH)
+model = BlipForConditionalGeneration.from_pretrained(MODEL_PATH, config=config, torch_dtype=torch.float16).to("cuda")
+
+img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
+raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+
+# conditional image captioning
+text = "a photography of"
+inputs = processor(raw_image, text, return_tensors="pt").to("cuda", torch.float16)
+
+out = model.generate(**inputs)
+print(processor.decode(out[0], skip_special_tokens=True))
+# >>> a photography of a woman and her dog
+
+# unconditional image captioning
+inputs = processor(raw_image, return_tensors="pt").to("cuda", torch.float16)
+
+out = model.generate(**inputs)
+print(processor.decode(out[0], skip_special_tokens=True))
+# >>> a woman sitting on the beach with her dog
